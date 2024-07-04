cd72856d016815374e2c4ffb287b5fe1e75ee8de
mois452b <moisesgabrielrodriguezruiz@gmail.com>
2024-07-04 19:06:12 -0400

    agregado validaciones de logueo para rutas

--- src/layout/appLayout.jsx
+++ src/layout/appLayout.jsx
@@ -1,11 +1,28 @@
+import { Navigate, useLocation, useNavigate } from "react-router-dom";
 import { AsideMenu } from "../components/asideMenu"
+import { userIsLoggedIn, userType } from "../services/login";
 
 export const AppLayout = ({ children }) => {
+
+    const { pathname } = useLocation()
+
+    const isLoggedIn = userIsLoggedIn()
+    const isAdmin = userType() === 'admin'
+
+    if( !isLoggedIn && pathname !== "/login" && pathname !== "/register" ) {
+        console.log("entro")
+        return <Navigate to="/login" />
+    }
+
+    if(!isLoggedIn) {
+        return children
+    }
+
     return (
         <div className="flex h-screen">
-            <aside className="w-1/4 bg-gray-200 p-4">
+            {isLoggedIn && isAdmin && <aside className="w-1/4 bg-gray-200 p-4">
                 <AsideMenu />
-            </aside>
+            </aside>}
             <main className="w-3/4 bg-white p-8">
                 {children}
             </main>
